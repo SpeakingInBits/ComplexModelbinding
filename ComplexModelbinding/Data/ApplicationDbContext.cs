57df7430a652e4439bcb4286d1a1cb8be2704741
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2021-10-10 12:49:04 -0700

    Add project files.

---
+++ ComplexModelbinding/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ComplexModelbinding.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComplexModelbinding.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Instructor> Instructors { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ ComplexModelbinding/Models/Instructor.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComplexModelbinding.Models
+{
+    public class Instructor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
